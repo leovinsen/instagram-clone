1545147b8d5c313b7298d86e9b13bd607133ce9f
vagrant <vagrant@homestead>
2020-03-19 12:41:34 +0000

    Refactors home.blade.php into its own folder

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -7,7 +7,11 @@
             <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2" class="rounded-circle col-12">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>{{ $user->username }}</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline">
+                <h1>{{ $user->username }}</h1>
+                <a href="$">Add New Post</a>
+            </div>
+
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
