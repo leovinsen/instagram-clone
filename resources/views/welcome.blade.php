fbed1aab0b102a799145b323637e9aa2559e8cc1
vagrant <vagrant@homestead>
2020-03-19 09:01:23 +0000

    Adds auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    InstagramClone
                 </div>
 
                 <div class="links">
