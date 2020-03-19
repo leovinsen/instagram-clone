7003db326bbf1b795911026fc321d79abebca7a9
vagrant <vagrant@homestead>
2020-03-19 11:14:08 +0000

    Adds a new field 'username' to table Users. Registration now requires a username.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,21 +2,32 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+    <div class="row">
+        <div class="col-3 p-5">
+            <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2" class="rounded-circle col-12">
+        </div>
+        <div class="col-9 pt-5">
+            <div><h1>Instagram Clone</h1></div>
+            <div class="d-flex">
+                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>23k</strong> followers</div>
+                <div class="pr-5"><strong>212</strong> following</div>
             </div>
+            <div class="pt-4 font-weight-bold">freeCodeCamp.org</div>
+            <div>We're a global community of millions of people learning to code together. We're an open source, donor-supported, 501(c)(3) nonprofit.</div>
+            <div><a href="#">www.freecodecamp.org</a></div>
+        </div>
+    </div>
+
+    <div class="row pt-4">
+        <div class="col-4">
+            <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2" class="w-100">
+        </div>
+        <div class="col-4">
+            <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2" class="w-100">
         </div>
     </div>
 </div>
