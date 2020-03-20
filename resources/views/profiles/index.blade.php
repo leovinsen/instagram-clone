4ae55639ec4faa849cde4022ef891e07e1d82702
Leonardo Vinsen <leonardovinsen@gmail.com>
2020-03-20 21:48:05 +0700

    Profile.index now shows the user's profile image

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
     <div class="row">
         <div class="col-3 p-5">
-            <img src="https://instagram.fcgk18-1.fna.fbcdn.net/v/t51.2885-19/s320x320/83213956_3360255157381124_5752385570823208960_n.jpg?_nc_ht=instagram.fcgk18-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=iyNgkh1HkjQAX8Ao1x2&oh=390cb247f37d15568deb368df88f2491&oe=5E9F1CC2"
-                class="rounded-circle col-12">
+            <img src="/storage/{{  $user->profile->image }}"
+                class="rounded-circle w-100">
         </div>
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline">
