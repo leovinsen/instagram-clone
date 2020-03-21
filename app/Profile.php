e5431f7f9c08ce7eaeb58d563d3407f7b488cf79
vagrant <vagrant@homestead>
2020-03-21 12:09:22 +0000

    Adds a convenience method to return a default 'No image available' if there is no image

--- app/Profile.php
+++ app/Profile.php
@@ -9,6 +9,12 @@ class Profile extends Model
 
     protected $guarded = [];
 
+    public function profileImage()
+    {
+        $imagePath = $this->image ??  'profile/qpqL07ZHiXpAFiFvOyJnmasV3uAp96U4EjVVfLw8.jpeg';
+        return '/storage/' . $imagePath;
+    }
+
     public function user()
     {
         return $this->belongsTo(User::class);
