cfba8b8442eacaeca1e4320312dc824b9e5b3d7d
vagrant <vagrant@homestead>
2020-03-21 15:50:05 +0000

    Sends a welcome email when user first registers -- powered by mailtrap.io

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+# Welcome to InstagramClone
+
+This is a clone of Instagram (obviously), based on FreeCodeCamp's Youtube Video **"Laravel PHP Framework Tutorial - Full Course for Beginners (2019)"**.
+<br>
+<br>
+Nothing fancy here -- just a small project to learn more about Laravel, which actually is an amazing framework!
+I was quite skeptical at first but I'm glad that I'm wrong.
+Anyways, enjoy your stay!
+
+Regards,<br>
+Leonardo
+
+
+@component('mail::button', ['url' => 'https://www.youtube.com/watch?v=ImtZ5yENzgE'])
+Watch Original Video
+@endcomponent
+@endcomponent
